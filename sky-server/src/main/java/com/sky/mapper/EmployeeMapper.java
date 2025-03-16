fe6376e660d906acd58e33c1382dff1e45b88600
cloog <395773508@qq.com>
2025-03-16 13:33:48 +0800

    完成员工分页查询接口的开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -10,6 +12,7 @@ public interface EmployeeMapper {
 
     /**
      * 根据用户名查询员工
+     *
      * @param username
      * @return
      */
@@ -17,7 +20,15 @@ public interface EmployeeMapper {
 
     /**
      * 插入员工
+     *
      * @param employee
      */
     void insert(Employee employee);
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO 查询条件
+     * @return Page 分页查询结果
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
